f8bf09c0c632b7ba3ba79f24bb699d21bdd10c94
Daniel Yang <shu.minyang@hotmail.com>
2023-07-12 11:32:32 -0400

    cleaning up

--- src/main.rs
+++ src/main.rs
@@ -6,19 +6,5 @@ mod day_two;
 mod utils;
 
 fn main() {
-    let a = "2-4,6-8";
-    let intervals = a
-        .split(",")
-        .map(|x| {
-            x.split("-")
-                .map(|x| x.parse::<usize>().expect("Invalid interval"))
-                .collect::<Vec<usize>>()
-        })
-        .collect::<Vec<Vec<usize>>>();
-    for interval in intervals {
-        for i in interval {
-            println!("{}", i)
-        }
-    }
     println!("Hello, world!");
 }
