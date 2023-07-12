46a40fd92288f7128c4a578a362fe71506ac8e40
Daniel Yang <shu.minyang@hotmail.com>
2023-07-12 09:59:17 -0400

    finished day_four

--- src/day_four/cleanup.rs
+++ src/day_four/cleanup.rs
@@ -1,31 +1,52 @@
+use std::ops::RangeInclusive;
+
 use itertools::Itertools;
 
 use crate::utils::read_file;
 
+trait ValidateRange {
+    fn contains_range(&self, other: &Self) -> bool;
+
+    fn contained_or_is_contained(&self, other: &Self) -> bool {
+        self.contains_range(other) || other.contains_range(self)
+    }
+}
+
+impl<T> ValidateRange for RangeInclusive<T>
+where
+    T: PartialOrd,
+{
+    fn contains_range(&self, other: &Self) -> bool {
+        self.contains(other.start()) && self.contains(other.end())
+    }
+}
+
 #[allow(dead_code)]
 fn cleanup() -> usize {
     let input = read_file("src/day_four/input.txt");
-    let a = input
+    input
         .lines()
         .map(|line| {
-            // line looks like this: 2-4
-            line.split("-")
-                .map(|digit| {
-                    digit
-                        .parse::<usize>()
-                        .expect("Invalid interval, digit is not parseable")
+            // line looks like this: 2-4,6-8
+            line.split(",")
+                .map(|interval| {
+                    // interval looks like this 2-4
+                    interval
+                        .split("-")
+                        .map(|digit| {
+                            digit
+                                .parse::<usize>()
+                                .expect("Invalid interval, digit is not parseable")
+                        })
+                        .collect_tuple::<(_, _)>()
+                        .map(|(start, end)| start..=end)
+                        .expect("Each interval should have a start and end")
                 })
                 .collect_tuple::<(_, _)>()
-                .map(|(start, end)| start..=end)
-                .expect("Each interval should have a start and end")
+                .expect("Each line must have a pair of intervals")
         })
-        .collect::<Vec<_>>();
-
-    for aa in a {
-        println!("{:?}", aa)
-    }
-
-    0
+        .filter(|(start, end)| start.contained_or_is_contained(&end))
+        .count()
 }
 
 #[cfg(test)]
