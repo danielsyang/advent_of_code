ea6ccb88980d214c16f1bda48dbdef861d6e7e5d
Daniel Yang <shu.minyang@hotmail.com>
2023-07-14 18:13:29 -0400

    day_five

---
+++ src/day_five/stack.rs
@@ -0,0 +1,14 @@
+#[allow(dead_code)]
+fn stack() -> String {
+    todo!("missing day_five: parsing exercise")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn stack_test() {
+        assert_eq!(stack(), "CMZ");
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod day_four;
 mod day_one;
 mod day_three;
 mod day_two;
+mod day_five;
 
 mod utils;
 
